67359c4265e60782ce16236dc2e8bdb8045b79fa
noelfoka <nomgnenoel@gmail.com>
2023-09-26 17:35:34 +0100

    learn array

--- array.rb
+++ array.rb
@@ -1,5 +1,8 @@
 #array1 = [-3, -2, -1, 0, 1, 2, 3]
 #print array1
 
-sea_creatures = ['shark', 'cuttlefish', 'squid', 'mantis shrimp']
-print sea_creatures
+#sea_creatures = ['shark', 'cuttlefish', 'squid', 'mantis shrimp']
+#print sea_creatures
+
+nom = gets
+print("salut #{nom}")
