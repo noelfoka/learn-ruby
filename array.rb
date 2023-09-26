b3c60a655402bb642a2d5255a03abb4c30f5d901
noelfoka <nomgnenoel@gmail.com>
2023-09-26 16:21:08 +0100

    update

---
+++ array.rb
@@ -0,0 +1,5 @@
+#array1 = [-3, -2, -1, 0, 1, 2, 3]
+#print array1
+
+sea_creatures = ['shark', 'cuttlefish', 'squid', 'mantis shrimp']
+print sea_creatures
--- dataType.rb
+++ dataType.rb
@@ -1,2 +1,8 @@
-sum = 5 + 4
-print sum
+#sum = 5 + 4
+#print sum
+
+#result = 5 > 8
+#print result
+
+text = "Hello, my name is Noel NOMGNE FOKA"
+print text
