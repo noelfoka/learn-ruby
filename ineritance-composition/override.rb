a3eb1260f302da42b4890e56a2411380b1a990e9
noelfoka <nomgnenoel@gmail.com>
2023-10-04 04:41:23 +0100

    implement override

---
+++ ineritance-composition/override.rb
@@ -0,0 +1,19 @@
+class Parent
+
+    def override()
+        puts 'PARENT override()'
+    end
+end
+
+class Child < Parent
+
+    def override()
+        puts 'CHILD override()'
+    end
+end
+
+dad = Parent.new()
+son = Child.new()
+
+dad.override()
+son.override()
