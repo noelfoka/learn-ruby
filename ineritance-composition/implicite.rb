96ba4c19796088fef2a88b752efbf2cb2761ea44
noelfoka <nomgnenoel@gmail.com>
2023-10-04 04:05:29 +0100

    learn inheritance

---
+++ ineritance-composition/implicite.rb
@@ -0,0 +1,16 @@
+class Parent
+
+    def implicit()
+        puts 'PARENT implicit()'
+    end
+end
+
+class Child < Parent
+
+end
+
+dad = Parent.new()
+son = Child.new()
+
+dad.implicit()
+son.implicit()
